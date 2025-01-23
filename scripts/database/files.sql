4bc5acac2441682bbc211d1a2fddc337489e65b3
gvcastellain <gabriel.castellain@gmail.com>
2025-01-23 00:09:49 -0300

    DB scripts

---
+++ scripts/database/files.sql
@@ -0,0 +1,18 @@
+CREATE TABLE files {
+    id serial,
+    fodler_id INT, 
+    owner_id INT NOT NULL,
+    name VARCHAR(200) NOT NULL, 
+    type VARCHAR(50) NOT NULL,
+    path VARCHAR(250) NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    modified_at TIMESTAMP NOT NULL,
+    deleted BOOL NOT NULL DEFAULT false,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_folders
+        FOREIGN KEY(fodler_id)
+            references folders(id),
+    CONSTRAINT fk_owner
+        FOREIGN KEY(owner_id)
+            REFERENCES user(id)
+}
---
+++ scripts/database/folders.sql
@@ -0,0 +1,12 @@
+CREATE TABLE folders {
+    id SERIAL,
+    parent_id INT, 
+    name varchar(60) NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp, 
+    modified_at TIMESTAMP NOT NULL, 
+    deleted BOOL NOT NULL DEFAULT false,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_parent
+    FOREIGN KEY(parent_id)
+    references folders(id)
+}
