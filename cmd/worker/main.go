50ee0fd56fab30310fcdefa48458f03a8e1e5cab
gvcastellain <gabriel.castellain@gmail.com>
2025-01-30 21:17:33 -0300

    worker ready

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"log"
 	"os"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/gvcastellain/go-driver/internal/bucket"
 	"github.com/gvcastellain/go-driver/internal/queue"
 )
 
@@ -18,5 +26,65 @@ func main() {
 
 	if err != nil {
 		panic(err) //todo
+	}
+
+	c := make(chan queue.QueueDto)
+	qc.Consume(c)
+
+	bcfg := bucket.AwsConfig{
+		Config: &aws.Config{
+			Region:      aws.String(os.Getenv("AWS_REGION")),
+			Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_KEY"), os.Getenv("AWS_SECRET"), ""),
+		},
+		BucketDownload: "go-drive-raw",
+		BucketUpload:   "go-drive-gzip",
+	}
+
+	b, err := bucket.New(bucket.AwsProvider, bcfg)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for msg := range c {
+		src := fmt.Sprintf("%d_%s", msg.Path, msg.Filename)
+		dst := fmt.Sprintf("%d_%s", msg.ID, msg.Filename)
+
+		file, err := b.Download(src, dst)
+		if err != nil {
+			log.Printf("error: %v", err)
+			continue
+		}
+
+		body, err := io.ReadAll(file)
+		if err != nil {
+			log.Printf("error: %v", err)
+			continue
+		}
+
+		var buf bytes.Buffer
+		zw := gzip.NewWriter(&buf)
+		_, err = zw.Write(body)
+		if err != nil {
+			log.Printf("error: %v", err)
+			continue
+		}
+
+		if err := zw.Close(); err != nil {
+			log.Printf("error: %v", err)
+			continue
+		}
+
+		zr, err := gzip.NewReader(&buf)
+		if err != nil {
+			log.Printf("error: %v", err)
+			continue
+		}
+
+		if b.Upload(zr, src); err != nil {
+			log.Printf("error: %v", err)
+			continue
+		}
+
 	}
 }
